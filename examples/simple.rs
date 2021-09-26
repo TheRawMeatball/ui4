2861094fd1d9ecb814a012efc3ab2240ff7f6c6d
TheRawMeatball <therawmeatball@gmail.com>
2021-09-26 20:15:39 +0300

    codedump (again)

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,10 +1,11 @@
-use std::borrow::Borrow;
 use bevy::prelude::*;
-use ui4_2::UiVal;
-use ui4_2::{init_ui, res, Ctx, ObserverExt, Ui4Plugin};
+use std::borrow::Borrow;
+use ui4::{init_ui, res, Ctx, ObserverExt, Ui4Plugin};
+use ui4::{ButtonFunc, IntoObservable};
 
 struct UiAssets {
     background: Handle<ColorMaterial>,
+    button: Handle<ColorMaterial>,
     text_style: TextStyle,
 }
 
@@ -15,6 +16,7 @@ fn init_system(
 ) {
     commands.insert_resource(UiAssets {
         background: assets.add(Color::BLACK.into()),
+        button: assets.add(Color::GRAY.into()),
         text_style: TextStyle {
             color: Color::WHITE,
             font: asset_server.load("FiraMono-Medium.ttf"),
@@ -27,18 +29,25 @@ fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins)
         .add_plugin(Ui4Plugin)
-        //.add_plugin(bevy_inspector_egui::WorldInspectorPlugin::default())
-        .add_startup_system(init_system);
+        .add_plugin(bevy_inspector_egui::WorldInspectorPlugin::default())
+        .add_startup_system(init_system)
+        .add_startup_system(
+            (|world: &mut World| init_ui(world, root))
+                .exclusive_system()
+                .at_end(),
+        );
 
     app.world.spawn().insert_bundle(UiCameraBundle::default());
-    init_ui(&mut app.world, root);
 
     app.run()
 }
 
 fn root(ctx: &mut Ctx) {
-    ctx.insert_bundle(NodeBundle::default())
-        .insert(Style {
+    #[derive(Component)]
+    struct State(i32);
+
+    ctx.with_bundle(NodeBundle::default())
+        .with(Style {
             size: Size {
                 width: Val::Percent(100.),
                 height: Val::Percent(100.),
@@ -46,24 +55,72 @@ fn root(ctx: &mut Ctx) {
             flex_direction: FlexDirection::ColumnReverse,
             ..Default::default()
         })
-        .insert(res::<UiAssets>().map(|assets| assets.background.clone()))
-        .static_child(text("Hello!".into()))
-        .static_child(text("How are you doing?".into()));
+        .with(res().map(|assets: &UiAssets| assets.background.clone()))
+        .with(State(0))
+        .static_child(text("Hello!".to_string()))
+        .static_child(text("How are you doing?".to_string()));
+
+    let state = ctx.component();
+    let this = ctx.this();
+    ctx.static_child(button(
+        "Increment".to_string(),
+        ButtonFunc::new(move |world| {
+            world.get_mut::<State>(this).unwrap().0 += 1;
+        }),
+    ))
+    .static_child(button(
+        "Decrement".to_string(),
+        ButtonFunc::new(move |world| {
+            world.get_mut::<State>(this).unwrap().0 -= 1;
+        }),
+    ))
+    .static_child(text(
+        state.map(|s: &State| format!("The number is {}", s.0)),
+    ));
+
+    ctx.static_child(text(
+        res()
+            .map(|time: &Time| time.seconds_since_startup() as usize % 2 == 0)
+            .map(|b| {
+                if b {
+                    "Now you see me :)"
+                } else {
+                    "Now you don't"
+                }
+            })
+            .map(ToString::to_string),
+    ));
 }
 
-fn text<M>(text: impl UiVal<String, M>) -> impl Fn(&mut Ctx) {
-    move |ctx| {
-        ctx.insert_bundle(TextBundle::default())
-            .insert(Style {
+fn text<O: IntoObservable<String, M>, M>(text: O) -> impl Fn(&mut Ctx) {
+    move |ctx: &mut Ctx| {
+        ctx.with_bundle(TextBundle::default())
+            .with(Style {
                 align_self: AlignSelf::FlexStart,
                 ..Default::default()
             })
-            .insert(
-                res::<UiAssets>()
-                    .and(text.clone().as_observable())
-                    .map(move |(assets, text)| {
-                        Text::with_section(text, assets.text_style.clone(), Default::default())
-                    }),
-            );
+            .with(res().and(text.clone().into_observable()).map(
+                move |(assets, text): (&UiAssets, O::ObserverReturn<'_, '_>)| {
+                    Text::with_section(text.borrow(), assets.text_style.clone(), Default::default())
+                },
+            ));
+    }
+}
+
+fn button<O: IntoObservable<String, M>, M: 'static>(
+    t: O,
+    button_func: ButtonFunc,
+) -> impl Fn(&mut Ctx) {
+    move |ctx: &mut Ctx| {
+        ctx.with_bundle(ButtonBundle::default())
+            .with(Style {
+                size: Size::new(Val::Px(150.0), Val::Px(65.0)),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                ..Default::default()
+            })
+            .with(res().map(|assets: &UiAssets| assets.button.clone()))
+            .with(button_func.clone())
+            .static_child(text(t.clone()));
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,19 @@
-#![feature(associated_type_bounds)]
+#![feature(generic_associated_types)]
+#![feature(unboxed_closures)]
 
 use std::borrow::Borrow;
+use std::fmt::Debug;
 use std::hash::Hash;
 use std::marker::PhantomData;
+use std::panic::Location;
 use std::sync::atomic::AtomicBool;
 use std::sync::Arc;
 
 use bevy::ecs::world::EntityMut;
 use bevy::ecs::{component::Component, prelude::*};
 use bevy::prelude::{BuildWorldChildren, DespawnRecursiveExt, Plugin};
-use bevy::utils::HashSet;
+use bevy::ui::Interaction;
+use bevy::utils::{HashMap, HashSet};
 
 #[derive(Default)]
 struct UiScratchSpace {
@@ -17,6 +21,7 @@ struct UiScratchSpace {
 }
 
 struct ResUpdateFuncs<T>(Vec<UpdateFunc>, PhantomData<T>);
+struct ComponentUpdateFuncs<T>(HashMap<Entity, Vec<UpdateFunc>>, PhantomData<T>);
 
 struct UiManagedSystems(SystemStage);
 
@@ -24,8 +29,33 @@ pub struct Ui4Plugin;
 impl Plugin for Ui4Plugin {
     fn build(&self, app: &mut bevy::prelude::App) {
         app.init_resource::<UiScratchSpace>()
+            .init_resource::<ButtonSystemState>()
             .insert_resource(UiManagedSystems(SystemStage::parallel()))
             .add_system(primary_ui_system.exclusive_system().at_end());
+    }
+}
+
+#[derive(Component, Clone)]
+pub struct ButtonFunc(Arc<dyn Fn(&mut World) + Send + Sync>);
+impl ButtonFunc {
+    pub fn new(f: impl Fn(&mut World) + Send + Sync + 'static) -> Self {
+        Self(Arc::new(f))
+    }
+    fn run(&self, world: &mut World) {
+        (self.0)(world)
+    }
+}
+
+struct ButtonSystemState {
+    query: QueryState<(&'static ButtonFunc, &'static Interaction), Changed<Interaction>>,
+    button_list: Vec<ButtonFunc>,
+}
+impl FromWorld for ButtonSystemState {
+    fn from_world(world: &mut World) -> Self {
+        Self {
+            query: world.query_filtered(),
+            button_list: vec![],
+        }
     }
 }
 
@@ -36,40 +66,71 @@ pub fn init_ui(world: &mut World, root: impl Fn(&mut Ctx)) {
     })
 }
 
-fn change_track_system<T: Send + Sync + 'static>(
+fn resource_change_track_system<T: Send + Sync + 'static>(
     mut ui: ResMut<UiScratchSpace>,
     mut update_funcs: ResMut<ResUpdateFuncs<T>>,
     detector: Res<T>,
 ) {
     if detector.is_changed() {
-        let list = &mut update_funcs.0;
-        let mut i = 0usize;
-        loop {
-            if i == list.len() {
-                break;
-            }
-            let relevant_uf = &list[i];
-            if relevant_uf.flagged() {
-                list.swap_remove(i);
-            } else {
-                ui.update_hashset.insert(relevant_uf.clone());
-                i += 1;
-            }
+        process_update_func_list(&mut update_funcs.0, &mut ui)
+    }
+}
+
+fn component_change_track_system<T: Component>(
+    mut ui: ResMut<UiScratchSpace>,
+    mut update_funcs: ResMut<ComponentUpdateFuncs<T>>,
+    detector: Query<Entity, Changed<T>>,
+) {
+    for entity in detector.iter() {
+        if let Some(list) = update_funcs.0.get_mut(&entity) {
+            process_update_func_list(list, &mut ui);
+        }
+    }
+}
+
+fn process_update_func_list(list: &mut Vec<UpdateFunc>, ui: &mut UiScratchSpace) {
+    let mut i = 0usize;
+    loop {
+        if i == list.len() {
+            break;
+        }
+        let relevant_uf = &list[i];
+        if relevant_uf.flagged() {
+            list.swap_remove(i);
+        } else {
+            ui.update_hashset.insert(relevant_uf.clone());
+            i += 1;
         }
     }
 }
 
 fn primary_ui_system(world: &mut World) {
+    world.resource_scope(|world, mut buttons: Mut<ButtonSystemState>| {
+        let buttons = &mut *buttons;
+        buttons
+            .button_list
+            .extend(buttons.query.iter(world).filter_map(
+                |(func, interaction)| match interaction {
+                    Interaction::Clicked => Some(func.clone()),
+                    Interaction::Hovered => None,
+                    Interaction::None => None,
+                },
+            ));
+
+        for func in buttons.button_list.drain(..) {
+            func.run(world);
+        }
+    });
     world.resource_scope(|world, mut systems: Mut<UiManagedSystems>| {
         systems.0.run(world);
-        world.resource_scope(|world, mut ui: Mut<UiScratchSpace>| {
-            let ui = &mut *ui;
-            for uf in ui.update_hashset.iter() {
-                uf.run(world);
-            }
+    });
+    world.resource_scope(|world, mut ui: Mut<UiScratchSpace>| {
+        let ui = &mut *ui;
+        for uf in ui.update_hashset.iter() {
+            uf.run(world);
+        }
 
-            ui.update_hashset.clear();
-        });
+        ui.update_hashset.clear();
     });
 }
 
@@ -78,13 +139,24 @@ pub struct Ctx<'a> {
     current_entity: Entity,
 }
 
+pub struct ComponentObserver<T: Send + Sync + 'static>(Entity, PhantomData<T>);
+
+impl<T: Send + Sync + 'static> Clone for ComponentObserver<T> {
+    fn clone(&self) -> Self {
+        Self(self.0, PhantomData)
+    }
+}
+
+impl<T: Send + Sync + 'static> Copy for ComponentObserver<T> {}
+
 impl Ctx<'_> {
-    pub fn insert<T: Component, M>(&mut self, item: impl UiVal<T, M>) -> &mut Self {
+    #[track_caller]
+    pub fn with<T: Component, M>(&mut self, item: impl Insertable<T, M>) -> &mut Self {
         item.insert_ui_val(self);
         self
     }
 
-    pub fn insert_bundle(&mut self, bundle: impl Bundle) -> &mut Self {
+    pub fn with_bundle(&mut self, bundle: impl Bundle) -> &mut Self {
         self.current_entity().insert_bundle(bundle);
         self
     }
@@ -104,9 +176,11 @@ impl Ctx<'_> {
         self
     }
 
-    pub fn optional_child<O>(&mut self, f: impl Fn(&mut Ctx) + Send + Sync + 'static, o: O)
+
+    #[rustfmt::skip]
+    pub fn if_child<O>(&mut self, o: O, t: impl Fn(&mut Ctx) + Send + Sync + 'static)
     where
-        for<'s, 'w> O: Observable<'s, 'w, Return = bool>,
+        for<'w, 's> O: Observable<Return<'w, 's> = bool>,
     {
         let observer = Arc::new(o);
         let observer_clone = observer.clone();
@@ -121,6 +195,7 @@ impl Ctx<'_> {
                 .map(|node| match node {
                     ChildNodeGroupKind::StaticChildren(len) => *len,
                     ChildNodeGroupKind::Optional(node, _) => node.is_some() as usize,
+                    ChildNodeGroupKind::IfElse(_, _, _) => 1,
                 })
                 .sum();
             let mut node = match &mut list[group_index] {
@@ -131,7 +206,7 @@ impl Ctx<'_> {
                 // spawn the node
                 (None, true) => {
                     let new_entity = world.spawn().id();
-                    f(&mut Ctx {
+                    t(&mut Ctx {
                         current_entity: new_entity,
                         world,
                     });
@@ -156,8 +231,78 @@ impl Ctx<'_> {
         });
 
         list.push(ChildNodeGroupKind::Optional(None, marker));
-
+        uf.run(&mut self.world);
         observer.register_self(self.world, uf);
+    }
+
+    #[rustfmt::skip]
+    pub fn if_else_child<O>(
+        &mut self,
+        o: O,
+        t: impl Fn(&mut Ctx) + Send + Sync + 'static,
+        f: impl Fn(&mut Ctx) + Send + Sync + 'static,
+    ) where
+        for<'w, 's> O: Observable<Return<'w, 's> = bool>,
+    {
+        let observer = Arc::new(o);
+        let observer_clone = observer.clone();
+        let parent = self.current_entity;
+        let child = self.world.spawn().id();
+        let list = self.get_child_tracker_list();
+        let group_index = list.len();
+        let (uf, marker) = UpdateFunc::new::<ChildNodeUpdateFuncMarker, _>(move |world| {
+            let new_value = observer_clone.get(world);
+            let list = get_marker_list(world.entity_mut(parent));
+            let index = list[..group_index]
+                .iter()
+                .map(|node| match node {
+                    ChildNodeGroupKind::StaticChildren(len) => *len,
+                    ChildNodeGroupKind::Optional(node, _) => node.is_some() as usize,
+                    ChildNodeGroupKind::IfElse(_, _, _) => 1,
+                })
+                .sum();
+            let last = match &mut list[group_index] {
+                ChildNodeGroupKind::IfElse(last, _, _) => last,
+                _ => unreachable!(),
+            };
+            let last_b = last.unwrap_or(!new_value);
+            if last_b != new_value {
+                *last = Some(new_value);
+                let current_entity = world.spawn().id();
+                let ctx = &mut Ctx {
+                    current_entity,
+                    world,
+                };
+                if new_value {
+                    t(ctx);
+                } else {
+                    f(ctx);
+                }
+                world
+                    .entity_mut(parent)
+                    .insert_children(index, &[current_entity]);
+                let list = get_marker_list(world.entity_mut(parent));
+                let e = match &mut list[group_index] {
+                    ChildNodeGroupKind::IfElse(_, e, _) => (e),
+                    _ => unreachable!(),
+                };
+                let entity = *e;
+                *e = current_entity;
+                world.entity_mut(entity).despawn_recursive();
+            }
+        });
+
+        list.push(ChildNodeGroupKind::IfElse(None, child, marker));
+        uf.run(&mut self.world);
+        observer.register_self(self.world, uf);
+    }
+
+    pub fn component<T: Send + Sync + 'static>(&self) -> ComponentObserver<T> {
+        ComponentObserver(self.current_entity, PhantomData)
+    }
+
+    pub fn this(&self) -> Entity {
+        self.current_entity
     }
 
     fn current_entity(&mut self) -> EntityMut<'_> {
@@ -185,31 +330,32 @@ struct ChildNodeUpdateFuncMarker;
 enum ChildNodeGroupKind {
     StaticChildren(usize),
     Optional(Option<Entity>, UfMarker<ChildNodeUpdateFuncMarker>),
+    IfElse(Option<bool>, Entity, UfMarker<ChildNodeUpdateFuncMarker>),
     // List(UfMarker<ChildNodeUpdateFuncMarker>),
 }
 
-#[derive(Default)]
+#[derive(Default, Component)]
 struct ManagedChildrenTracker {
     children: Vec<ChildNodeGroupKind>,
 }
 
-pub trait Observable<'s, 'w>: Clone + Send + Sync + 'static {
-    type Return;
+pub trait Observable: Clone + Send + Sync + 'static {
+    type Return<'w, 's>;
 
-    fn get(&'s self, world: &'w World) -> Self::Return;
+    fn get<'w, 's>(&'s self, world: &'w World) -> Self::Return<'w, 's>;
     fn register_self(&self, world: &mut World, uf: UpdateFunc);
 }
 
 #[derive(Clone)]
 pub struct Map<O, F>(O, F);
-impl<'s, 'w, O, F, R> Observable<'s, 'w> for Map<O, F>
+impl<O, F> Observable for Map<O, F>
 where
-    O: Observable<'s, 'w>,
-    F: Fn(O::Return) -> R + Clone + Send + Sync + 'static,
+    O: Observable,
+    F: for<'w, 's> Fn<(O::Return<'w, 's>,)> + Clone + Send + Sync + 'static,
 {
-    type Return = R;
+    type Return<'w, 's> = <F as FnOnce<(O::Return<'w, 's>,)>>::Output;
 
-    fn get(&'s self, world: &'w World) -> Self::Return {
+    fn get<'w, 's>(&'s self, world: &'w World) -> Self::Return<'w, 's> {
         (self.1)(self.0.get(world))
     }
 
@@ -220,14 +366,10 @@ where
 
 #[derive(Clone)]
 pub struct And<O1, O2>(O1, O2);
-impl<'s, 'w, O1, O2> Observable<'s, 'w> for And<O1, O2>
-where
-    O1: Observable<'s, 'w>,
-    O2: Observable<'s, 'w>,
-{
-    type Return = (O1::Return, O2::Return);
+impl<O1: Observable, O2: Observable> Observable for And<O1, O2> {
+    type Return<'w, 's> = (O1::Return<'w, 's>, O2::Return<'w, 's>);
 
-    fn get(&'s self, world: &'w World) -> Self::Return {
+    fn get<'w, 's>(&'s self, world: &'w World) -> Self::Return<'w, 's> {
         (self.0.get(world), self.1.get(world))
     }
 
@@ -245,10 +387,10 @@ impl<R> Clone for ResObserver<R> {
     }
 }
 
-impl<'s, 'w, R: Send + Sync + 'static> Observable<'s, 'w> for ResObserver<R> {
-    type Return = &'w R;
+impl<R: Send + Sync + 'static> Observable for ResObserver<R> {
+    type Return<'w, 's> = &'w R;
 
-    fn get(&'s self, world: &'w World) -> Self::Return {
+    fn get<'w, 's>(&'s self, world: &'w World) -> Self::Return<'w, 's> {
         world.get_resource::<R>().unwrap()
     }
 
@@ -257,37 +399,28 @@ impl<'s, 'w, R: Send + Sync + 'static> Observable<'s, 'w> for ResObserver<R> {
             if let Some(mut list) = world.get_resource_mut::<ResUpdateFuncs<R>>() {
                 list.0.push(uf);
             } else {
-                systems.0.add_system(change_track_system::<R>);
+                systems.0.add_system(resource_change_track_system::<R>);
                 world.insert_resource(ResUpdateFuncs::<R>(vec![uf], PhantomData));
             };
         });
     }
 }
 
-pub trait ObserverExt: for<'s, 'w> Observable<'s, 'w> + Sized {
-    fn map<F, R>(self, f: F) -> Map<Self, F>
+pub trait ObserverExt: Observable + Sized {
+    fn map<F>(self, f: F) -> Map<Self, F>
     where
-        F: for<'s, 'w> Fn(<Self as Observable<'s, 'w>>::Return) -> R + Send + Sync + 'static;
+        F: for<'w, 's> Fn<(<Self as Observable>::Return<'w, 's>,)> + Send + Sync + 'static;
     fn and<O>(self, o: O) -> And<Self, O>
     where
-        O: for<'s, 'w> Observable<'s, 'w>;
+        O: Observable;
 }
 
-impl<T> ObserverExt for T
-where
-    T: for<'s, 'w> Observable<'s, 'w> + Sized,
-{
-    fn map<F, R>(self, f: F) -> Map<Self, F>
-    where
-        F: for<'s, 'w> Fn(<T as Observable<'s, 'w>>::Return) -> R + Send + Sync + 'static,
-    {
+impl<T: Observable> ObserverExt for T {
+    fn map<F>(self, f: F) -> Map<Self, F> {
         Map(self, f)
     }
 
-    fn and<O>(self, o: O) -> And<Self, O>
-    where
-        O: for<'s, 'w> Observable<'s, 'w>,
-    {
+    fn and<O>(self, o: O) -> And<Self, O> {
         And(self, o)
     }
 }
@@ -296,12 +429,23 @@ pub fn res<R: Send + Sync + 'static>() -> ResObserver<R> {
     ResObserver(PhantomData)
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct UpdateFunc(Arc<UfInner<dyn Fn(&mut World) + Send + Sync>>);
 struct UfInner<F: ?Sized> {
     flag: AtomicBool,
+    created_at: &'static Location<'static>,
     func: F,
 }
+
+impl<F: ?Sized> Debug for UfInner<F> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("UfInner")
+            .field("created_at", &self.created_at)
+            .finish()
+    }
+}
+
+#[derive(Component)]
 struct UfMarker<T>(
     Arc<UfInner<dyn Fn(&mut World) + Send + Sync>>,
     PhantomData<T>,
@@ -316,9 +460,11 @@ impl<T> Drop for UfMarker<T> {
 }
 
 impl UpdateFunc {
+    #[track_caller]
     fn new<T, F: Fn(&mut World) + Send + Sync + 'static>(func: F) -> (Self, UfMarker<T>) {
         let arc = Arc::new(UfInner {
             flag: AtomicBool::new(false),
+            created_at: std::panic::Location::caller(),
             func,
         });
         (Self(arc.clone()), UfMarker(arc, PhantomData))
@@ -354,10 +500,9 @@ impl Ord for UpdateFunc {
     }
 }
 
-pub trait UiVal<T, M>: Clone + Send + Sync + 'static {
-    type Observable: for<'s, 'w> Observable<'s, 'w, Return = T>;
+pub trait Insertable<T, M>: Send + Sync + 'static {
+    #[track_caller]
     fn insert_ui_val(self, ctx: &mut Ctx);
-    fn as_observable(self) -> Self::Observable;
 }
 
 pub struct Static;
@@ -366,33 +511,27 @@ pub struct Dynamic;
 #[derive(Clone)]
 pub struct StaticObserver<T>(T);
 
-impl<'s, 'w, T: Component + Clone> Observable<'s, 'w> for StaticObserver<T> {
-    type Return = T;
+impl<T: Clone + Send + Sync + 'static> Observable for StaticObserver<T> {
+    type Return<'w, 's> = &'s T;
 
-    fn get(&'s self, _: &'w bevy::prelude::World) -> <Self as Observable<'s, 'w>>::Return {
-        self.0.clone()
+    fn get<'w, 's>(&'s self, _: &'w bevy::prelude::World) -> Self::Return<'w, 's> {
+        &self.0
     }
     fn register_self(&self, _: &mut bevy::prelude::World, _: UpdateFunc) {}
 }
 
-impl<T: Component + Clone> UiVal<T, Static> for T {
-    type Observable = StaticObserver<T>;
-
+impl<T: Component> Insertable<T, Static> for T {
     fn insert_ui_val(self, ctx: &mut Ctx<'_>) {
         ctx.current_entity().insert(self);
     }
-
-    fn as_observable(self) -> Self::Observable {
-        StaticObserver(self)
-    }
 }
 
-impl<T: Component, O> UiVal<T, Dynamic> for O
+#[rustfmt::skip]
+impl<T: Component, O> Insertable<T, Dynamic> for O
 where
-    for<'s, 'w> O: Observable<'s, 'w, Return = T>,
+    for<'w, 's> O: Observable<Return<'w, 's> = T>,
 {
-    type Observable = O;
-
+    #[track_caller]
     fn insert_ui_val(self, ctx: &mut Ctx<'_>) {
         let entity = ctx.current_entity;
         let observer = Arc::new(self);
@@ -402,10 +541,57 @@ where
             world.entity_mut(entity).insert(val);
         });
         ctx.current_entity().insert(marker);
+        uf.run(&mut ctx.world);
         observer.register_self(ctx.world, uf);
     }
+}
 
-    fn as_observable(self) -> Self::Observable {
+#[rustfmt::skip]
+pub trait IntoObservable<T, M>: Clone + Send + Sync + 'static
+{
+    type Observer: for<'w, 's> Observable<Return<'w, 's> = Self::ObserverReturn<'w, 's>>;
+    type ObserverReturn<'w, 's>: Borrow<T>;
+    fn into_observable(self) -> Self::Observer;
+}
+
+impl<T: Clone + Send + Sync + 'static> IntoObservable<T, Static> for T {
+    type Observer = StaticObserver<T>;
+    type ObserverReturn<'w, 's> = &'s T;
+
+    fn into_observable(self) -> Self::Observer {
+        StaticObserver(self)
+    }
+}
+
+#[rustfmt::skip]
+impl<T, O: for<'w, 's> Observable<Return<'w, 's> = T>> IntoObservable<T, Dynamic> for O {
+    type Observer = Self;
+    type ObserverReturn<'w, 's> = O::Return<'w, 's>;
+
+    fn into_observable(self) -> Self::Observer {
         self
+    }
+
+}
+
+impl<T: Component> Observable for ComponentObserver<T> {
+    type Return<'w, 's> = &'w T;
+
+    fn get<'w, 's>(&'s self, world: &'w World) -> Self::Return<'w, 's> {
+        &world.get::<T>(self.0).unwrap()
+    }
+
+    fn register_self(&self, world: &mut World, uf: UpdateFunc) {
+        world.resource_scope(|world, mut systems: Mut<UiManagedSystems>| {
+            if let Some(mut lists) = world.get_resource_mut::<ComponentUpdateFuncs<T>>() {
+                lists.0.entry(self.0).or_default().push(uf);
+            } else {
+                systems.0.add_system(component_change_track_system::<T>);
+                world.insert_resource(ComponentUpdateFuncs::<T>(
+                    [(self.0, vec![uf])].into_iter().collect(),
+                    PhantomData,
+                ));
+            };
+        });
     }
 }
