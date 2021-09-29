aa356e44856631866444a9a8771fea809b260df3
TheRawMeatball <therawmeatball@gmail.com>
2021-09-29 15:15:23 +0300

    some more small fixes

--- src/childable/tracked/map.rs
+++ src/childable/tracked/map.rs
@@ -1,5 +1,5 @@
 use std::collections::BTreeMap;
-use std::{marker::PhantomData, sync::Mutex};
+use std::sync::Mutex;
 
 use crossbeam_channel::Sender;
 
@@ -22,22 +22,6 @@ impl<K, V> Default for TrackedMap<K, V> {
 impl<K: Clone, V: Clone> TrackedMap<K, V> {
     pub fn new() -> Self {
         Self::default()
-    }
-}
-
-pub struct TrackedItemObserver<T: Send + Sync + 'static> {
-    _marker: PhantomData<T>,
-    group_index: usize,
-    index: usize,
-}
-
-impl<T: Send + Sync + 'static> Clone for TrackedItemObserver<T> {
-    fn clone(&self) -> Self {
-        Self {
-            _marker: PhantomData,
-            group_index: self.group_index,
-            index: self.index,
-        }
     }
 }
 
