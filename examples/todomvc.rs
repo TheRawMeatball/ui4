0df92d49d78fa3805810df230c5e9051c4f8931d
TheRawMeatball <therawmeatball@gmail.com>
2021-12-19 11:34:10 +0300

    improve todomvc

--- examples/todomvc.rs
+++ examples/todomvc.rs
@@ -33,7 +33,8 @@ fn root(ctx: Ctx) -> Ctx {
     ctx.with(UiColor(Color::BLACK))
         .with(Right(Units::Stretch(1.)))
         .with(Left(Units::Stretch(1.)))
-        .with(Width(Units::Percentage(60.)))
+        .with(Width(Units::Pixels(400.)))
+        .with(Height(Units::Pixels(600.)))
         .child(
             text("Todos")
                 .with(TextSize(80.))
@@ -46,9 +47,13 @@ fn root(ctx: Ctx) -> Ctx {
         .child(|ctx: Ctx| {
             ctx.with(Height(Units::Auto))
                 .with(LayoutType::Row)
+                .with(Left(Units::Pixels(5.)))
+                .with(Right(Units::Pixels(5.)))
+                .with(ColBetween(Units::Pixels(5.)))
+                .with(Bottom(Units::Pixels(5.)))
                 .child(
                     textbox(res().lens(EditedText::F0))
-                        .with(Width(Units::Percentage(90.)))
+                        .with(Width(Units::Stretch(9.)))
                         .with(Height(Units::Pixels(30.))),
                 )
                 .child(
@@ -62,7 +67,7 @@ fn root(ctx: Ctx) -> Ctx {
                                 done: false,
                             });
                         }))
-                        .with(Width(Units::Percentage(10.)))
+                        .with(Width(Units::Stretch(1.)))
                         .with(Height(Units::Pixels(30.))),
                 )
         })
@@ -75,29 +80,38 @@ fn root(ctx: Ctx) -> Ctx {
 
 fn todo(item: impl WorldLens<Out = Todo>, index: IndexObserver) -> impl FnOnce(Ctx) -> Ctx {
     move |ctx: Ctx| {
-        ctx.with(Width(Units::Percentage(100.)))
-            .with(Height(Units::Pixels(30.)))
+        ctx.with(Height(Units::Pixels(30.)))
             .with(LayoutType::Row)
-            .with(ColBetween(Units::Stretch(1.)))
+            .with(Left(Units::Pixels(5.)))
+            .with(Right(Units::Pixels(5.)))
+            .with(Bottom(Units::Pixels(2.)))
             .with(
                 item.map(|t: &Todo| t.done)
                     .dedup()
                     .map(|&done: &bool| if done { Color::GREEN } else { Color::NONE })
                     .map(UiColor),
             )
-            .child(text(item.map(|t: &Todo| t.text.to_string())).with(TextSize(32.)))
+            .child(
+                text(item.map(|t: &Todo| t.text.to_string()))
+                    .with(TextSize(28.))
+                    .with(TextAlign(TextAlignment {
+                        vertical: VerticalAlign::Top,
+                        horizontal: HorizontalAlign::Left,
+                    })),
+            )
             .children(
                 item.map(|todo: &Todo| todo.done)
                     .map_child(move |done: bool| {
                         move |ctx: &mut McCtx| {
                             if done {
                                 ctx.c(|ctx: Ctx| {
-                                    ctx.with(Width(Units::Pixels(250.)))
+                                    ctx.with(Width(Units::Pixels(150.)))
                                         .with(Height(Units::Pixels(30.)))
                                         .with(LayoutType::Row)
+                                        .with(ColBetween(Units::Pixels(5.)))
                                         .child(
                                             button("Unmark")
-                                                .with(Width(Units::Percentage(50.)))
+                                                .with(Width(Units::Stretch(1.)))
                                                 .with(Height(Units::Pixels(30.)))
                                                 .with(OnClick::new(move |world| {
                                                     item.get_mut(world).done = false;
@@ -105,7 +119,7 @@ fn todo(item: impl WorldLens<Out = Todo>, index: IndexObserver) -> impl FnOnce(C
                                         )
                                         .child(
                                             button("Remove")
-                                                .with(Width(Units::Percentage(50.)))
+                                                .with(Width(Units::Stretch(1.)))
                                                 .with(Height(Units::Pixels(30.)))
                                                 .with(index.dedup().map(|&i: &usize| {
                                                     OnClick::new(move |world| {
@@ -119,7 +133,7 @@ fn todo(item: impl WorldLens<Out = Todo>, index: IndexObserver) -> impl FnOnce(C
                                 });
                             } else {
                                 ctx.c(button("Mark Complete")
-                                    .with(Width(Units::Pixels(250.)))
+                                    .with(Width(Units::Pixels(150.)))
                                     .with(Height(Units::Pixels(30.)))
                                     .with(OnClick::new(move |world| {
                                         item.get_mut(world).done = true;
